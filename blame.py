b726912a9c6b78881c45d3f29b5563992856bcd4
evansalter <evanearlsalter@gmail.com>
2015-11-28 22:04:09 -0600

    Added contribution percentage chart

--- blame.py
+++ blame.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import subprocess
 import os
 import string
@@ -13,6 +15,20 @@ def zeroIfNull(s):
         return "0"
     else:
         return s
+
+def printGraph():
+    print "Percentages of lines added per user:"
+    for index in range(len(usernames)):
+        percent = float(zeroIfNull(inserted[index]))/totalInserted*100
+        # print usernames[index] + ": " + str(int(percent)) + "%"
+        print "[",
+        numSigns=0
+        for i in range(0, int(percent), 5):
+            print "=",
+            numSigns += 1
+        for i in range(numSigns, 20, 1):
+            print " ",
+        print "] " + str(int(percent)) + "%\t--> " + usernames[index]
 
 usernames = subprocess.check_output("git shortlog -s | cut -c8- | sort -u", shell=True).splitlines()
 
@@ -32,6 +48,9 @@ for index in range(len(usernames)):
     f = subprocess.check_output("echo \"" + output + "\" | cut -d ' ' -f1", shell=True).rstrip()
     i = subprocess.check_output("echo \"" + output + "\" | cut -d ' ' -f2", shell=True).rstrip()
     d = subprocess.check_output("echo \"" + output + "\" | cut -d ' ' -f3", shell=True).rstrip()
+    files.append(f)
+    inserted.append(i)
+    deleted.append(d)
 
     print "Files changed: " + zeroIfNull(f) + "; Lines inserted: " + zeroIfNull(i) + "; Lines deleted: " + zeroIfNull(d)
     print "---------------------------"
@@ -49,3 +68,7 @@ print "\nTotals:"
 print "Files changed: " + str(totalFiles)
 print "Lines inserted: " + str(totalInserted)
 print "Lines deleted: " + str(totalDeleted)
+
+print 
+
+printGraph()
